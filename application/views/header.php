7d5a6bb7f4d0a44729cf23c3d6d1df3d42c0fb12
James Martinez <amajamesmartinez@gmail.com>
2020-02-15 05:14:46 +0800

    HOTFIX: Broken Home Link

--- application/views/header.php
+++ application/views/header.php
@@ -1,12 +1,12 @@
 <nav class="navbar navbar-inverse navbar-fixed-top">
     <div class="container-fluid">
     	<div class="navbar-header">
-	      <a class="navbar-brand" href="<?php echo base_url() ?>/index.php/home" style="margin:10px;padding: 0px">
+	      <a class="navbar-brand" href="<?php echo base_url() ?>index.php/home" style="margin:10px;padding: 0px">
 	      	<img src="<?php echo base_url() ?>assets/img/logo.png" height="120%" width="auto">
 	      </a>
 	    </div>
 	    <div class="nav navbar-nav">
-	    	<h3 style="color:white"> <a style="color:white; text-decoration:none;" href="<?php echo base_url() ?>/index.php/home">AMA Computer College - Cavite | Web - Based Test Banking System</a> </h3>
+	    	<h3 style="color:white"> <a style="color:white; text-decoration:none;" href="<?php echo base_url() ?>index.php/home">AMA Computer College - Cavite | Web - Based Test Banking System</a> </h3>
 		</div>
 	</div>
 	<div class="container-fluid">
